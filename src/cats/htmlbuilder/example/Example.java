c0ffc35e96a944f0f4f22d4901fb05ce8d7ff3c1
Josh Maione <x_silentmurder@hotmail.com>
2013-10-12 01:11:30 -0400

    Update Example.java

--- src/cats/htmlbuilder/example/Example.java
+++ src/cats/htmlbuilder/example/Example.java
@@ -10,6 +10,7 @@ import static cats.htmlbuilder.page.Element.BODY;
 import static cats.htmlbuilder.page.Element.LI;
 import static cats.htmlbuilder.page.Element.P;
 import static cats.htmlbuilder.page.Element.UL;
+import static cats.htmlbuilder.page.Element.TAG;
 import static cats.htmlbuilder.page.stylesheet.Style.BACKGROUND_COLOR;
 import static cats.htmlbuilder.page.stylesheet.Style.CLASS;
 import static cats.htmlbuilder.page.stylesheet.Style.COLOR;
@@ -36,7 +37,7 @@ public class Example {
         list.append(LI, "Item 1").append(LI, "Item 2").append(LI, "Item 3");
         final Element nested = new Element(UL);
         nested.append(LI, "Nested Item 1").append(LI, "Nested Item 2").append(LI, "Nested Item 3");
-        list.child(Element.TAG, LI, 2).append(nested);
+        list.child(TAG, LI, 2).append(nested);
         list.append(LI, "Item 4").append(LI, "Item 5").append(LI, "Item 6");
         page.body().append(list);
         page.body().append(new Element(P, "This is a paragraph").attribute("class", "para"));
